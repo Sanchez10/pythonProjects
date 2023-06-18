2e48c4edc7ac352c8dfdfbc751c6300eeba71993
João Sanchez <joao.sanchez01@gmail.com>
2023-06-18 00:40:56 -0300

    [Password Manager]: Start a project of a new password manager

---
+++ password_manager/password_manager.py
@@ -0,0 +1,5 @@
+""""
+Design a password management application that securely stores and manages user credentials. 
+Implement features like encryption, password generation, and integration with a database. 
+This project will allow you to explore concepts such as encryption algorithms, data security, and user authentication. 
+"""
