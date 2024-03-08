7f13145ea2dc6af86e07a80813786d874ff64e10
João Sanchez <joao.sanchez01@gmail.com>
2024-03-08 18:49:37 -0300

    [PasswordManager]: Creating the login page with the images and the labels

--- password_manager/password_manager.py
+++ password_manager/password_manager.py
@@ -1,41 +1,52 @@
-import customtkinter
+import customtkinter as ctk
 from tkinter import *
 
-customtkinter.set_appearance_mode("dark")
-customtkinter.set_default_color_theme("dark-blue")
+window = ctk.CTk()
 
-window = customtkinter.CTk()
-window.geometry("700x410")
-window.title("Login Sytem")
-window.iconbitmap("password_code.ico")
-window.resizable("False", "False")
+class Application():
+    def __init__(self):
+        self.window = window
+        self.theme()
+        self.main_window()
+        self.login_window()
+        window.mainloop()
+    
+    def theme(self):
+        ctk.set_appearance_mode("dark")
+        ctk.set_default_color_theme("dark-blue")
 
-# Login window image
-img = PhotoImage(file="login.png")
-label_img = customtkinter.CTkLabel(master=window, image=img, text="")
-label_img.place(x=25, y=75)
+    def main_window(self):
+        window.geometry("700x410")
+        window.title("Login System")
+        window.iconbitmap("password_code.ico")
+        window.resizable("False", "False")
 
-label_tt = customtkinter.CTkLabel(master=window, text='Make login to acces your manager \npasswords', font=('Roboto', 18, 'bold'), text_color='#1F538D')
-label_tt.place(x=25, y=10)
+    def login_window(self):
+        # Login window image
+        img = PhotoImage(file="login.png")
+        label_img = ctk.CTkLabel(master=window, image=img, text="").place(x=25, y=75)
 
-# Frame
-frame = customtkinter.CTkFrame(master=window, width=350, height=406)
-frame.pack(side=RIGHT)
+        label_tt = ctk.CTkLabel(master=window, text='Make login to acces your manager \npasswords', font=('Roboto', 18, 'bold'), text_color='#1F538D').place(x=25, y=10)
 
-# Frame widgets
-label = customtkinter.CTkLabel(master=frame, text='Login', font = ('Roboto', 20, 'bold'), text_color= ('white') )
-label.place(x=25, y=10)
+        # Frame
+        login_frame = ctk.CTkFrame(master=window, width=320, height=260)
+        login_frame.pack(side=RIGHT, padx=25, pady=10)
 
-# User
-entry_user = customtkinter.CTkEntry(master=frame, placeholder_text='User', width=300)
-entry_user.place(x=25, y=105)
+        # Frame widgets
+        label = ctk.CTkLabel(master=login_frame, text='Login', font=('Roboto', 20, 'bold'), text_color= ('white') ).place(x=134, y=10)
 
-# Password
-entry_password = customtkinter.CTkEntry(master=frame, placeholder_text='Password', width=300)
-entry_password.place(x=25, y=155)
+        # User label
+        entry_user = ctk.CTkEntry(master=login_frame, placeholder_text='User', width=280).place(x=20, y=60)
 
-# Button
-button = customtkinter.CTkButton(master=frame, text='LOGIN', width=300)
-button.place(x=25, y=205)
+        # Password label
+        entry_password = ctk.CTkEntry(master=login_frame, placeholder_text='Password', width=280, show='*').place(x=20, y=110)
 
-window.mainloop()
+        # Login button
+        login_button = ctk.CTkButton(master=login_frame, text='LOGIN', width=280).place(x=20, y=160)
+
+        # Register span
+        register_span = ctk.CTkLabel(master=login_frame, text="If you don't have an account").place(x=20, y=210)
+        register_button = ctk.CTkButton(master=login_frame, fg_color='green', hover_color='#2D9334', text="Sign Up", width=110).place(x=190, y=210)
+
+if __name__ == "__main__":
+    Application()
