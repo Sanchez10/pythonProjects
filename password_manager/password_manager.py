bd893c51a82ac0bf967378976e44f4c87455e080
João Sanchez <joao.sanchez01@gmail.com>
2024-03-08 19:40:50 -0300

    [PasswordManager]: Create the register windows and buttons to back to login and save user. In the login page is possible to return a message indicating that user make the login, not worked with validation by now

--- password_manager/password_manager.py
+++ password_manager/password_manager.py
@@ -1,5 +1,6 @@
 import customtkinter as ctk
 from tkinter import *
+from tkinter import messagebox
 
 window = ctk.CTk()
 
@@ -41,12 +42,42 @@ class Application():
         # Password label
         entry_password = ctk.CTkEntry(master=login_frame, placeholder_text='Password', width=280, show='*').place(x=20, y=110)
 
+        def login_user():
+            msg = messagebox.showinfo(title="Status of enrollment", message="Congratulation user enrolled success!")
+
         # Login button
-        login_button = ctk.CTkButton(master=login_frame, text='LOGIN', width=280).place(x=20, y=160)
+        login_button = ctk.CTkButton(master=login_frame, text='LOGIN', width=280, command=login_user).place(x=20, y=160)
 
         # Register span
         register_span = ctk.CTkLabel(master=login_frame, text="If you don't have an account").place(x=20, y=210)
-        register_button = ctk.CTkButton(master=login_frame, fg_color='green', hover_color='#2D9334', text="Sign Up", width=110).place(x=190, y=210)
+        
+        def register_window():
+            # Remove frame login
+            login_frame.pack_forget()
+            
+            # Creating enrollment window
+            rg_frame = ctk.CTkFrame(master=window, width=320, height=260)
+            rg_frame.pack(side=RIGHT, padx=25, pady=10)
+            label = ctk.CTkLabel(master=rg_frame, text='Make your register', font=('Roboto', 20, 'bold'), text_color= ('white') ).place(x=75, y=10)
+            # span = ctk.CTkLabel(master=rg_frame, text="Fill labels correctly.", text_font=('Roboto', 10), text_color="gray").place(x=25, y=35)
+
+            entry_user = ctk.CTkEntry(master=rg_frame, placeholder_text='User', width=280).place(x=20, y=60)
+            email_user = ctk.CTkEntry(master=rg_frame, placeholder_text='Email', width=280).place(x=20, y=100)
+            entry_password = ctk.CTkEntry(master=rg_frame, placeholder_text='Password', width=280, show='*').place(x=20, y=140)
+            confirm_password = ctk.CTkEntry(master=rg_frame, placeholder_text='Confirm password', width=280, show='*').place(x=20, y=180)
+
+            def back_to_login():
+                rg_frame.pack_forget()
+                login_frame.pack(side=RIGHT, padx=25, pady=10)
+
+            back_button = ctk.CTkButton(master=rg_frame, text='Back', width=100, command=back_to_login).place(x=50, y=220)
+            
+            def save_user():
+                msg = messagebox.showinfo(title="Status of enrollment", message="Congratulation user enrolled success!")
+            
+            save_button = ctk.CTkButton(master=rg_frame, text='Register', width=100, fg_color="green", hover_color="#014B05", command=save_user).place(x=170, y=220)
+
+        register_button = ctk.CTkButton(master=login_frame, fg_color='green', hover_color='#2D9334', text="Sign Up", width=110, command=register_window).place(x=190, y=210)
 
 if __name__ == "__main__":
     Application()
