de36688312025377fc9544f0e331c1c1aec3c46b
João Sanchez <joao.sanchez01@gmail.com>
2025-03-01 23:21:19 -0300

    config: Python projects commits

---
+++ python-config/src/start.py
@@ -0,0 +1,18 @@
+def soma(num_1: int, num_2: int) -> int:
+    """Somando dois números
+    :param - num_1: primeiro número da soma
+           - num_2: segundo número da soma
+    :return - Soma
+    """
+    soma_dois_numeros = num_1 + num_2
+    return soma_dois_numeros
+
+
+def minha_funcao(
+    argumento_muito_grande1,
+    argumento_muito_grande2,
+    argumento_muito_grande3,
+    argumento_muito_grande4,
+):
+    print(argumento_muito_grande1)
+    return True
